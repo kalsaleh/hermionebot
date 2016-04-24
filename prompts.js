dfd427391bdf190846056d46aa596a42342f32a8
Veronica Cannon <me@veronicacannon.com>
2016-04-23 20:24:30 -0700

    Update data array values.

--- prompts.js
+++ prompts.js
@@ -9,7 +9,7 @@ module.exports = {
   topicUnknown: "I don't know abou t'%(topic)s'. Pick a topic from the list or say 'nevermind'.",
   topicMissing: "I didn't catch that, what topic did you want to know about?",
   cancel: "If you insist.",
-  answerDescription: "%(value).",
-  answerSnippet: "Here's an example of %(topic): \n %(value)",
-  answerUseCase: "You would use %(topic) in this case: \n%(value)."
+  answerDescription: "%(value)s",
+  answerSnippet: "Here's an example of %(topic): \n %(value)s",
+  answerUseCase: "You would use %(topic) in this case: \n%(value)s."
 };
