2dc27d63839bfa583272e392e9cc15330eb8f571
Veronica Cannon <me@veronicacannon.com>
2016-04-23 20:08:55 -0700

    Add askTopic and askQuestion methods.

--- prompts.js
+++ prompts.js
@@ -1,13 +1,14 @@
 module.exports = {
-  helpMessage: "Hermione can answer your programming questions! Here are just some of the questions she can answer for you:\n\n" +
+  helloMessage: "Hello, I'm Hermione",
+  helpMessage: "I can answer your programming questions! Here are just some of the questions I can answer for you:\n\n" +
   "* 'What is an array?'\n" +
   "* 'Show me an example of a function'\n" +
   "* 'When would I use an iterator?'\n" +
-  "\nGet ready to learn, and have fun!.",
+  "\nGet ready to learn, and have fun!",
   topicUnknown: "I don't know abou t'%(topic)s'. Pick a topic from the list or say 'nevermind'.",
   topicMissing: "I didn't catch that, what topic did you want to know about?",
   cancel: "If you insist.",
-  answerDefinition: "%(topic)s are: \n %(value).",
+  answerDescription: "%(value).",
   answerSnippet: "Here's an example of %(topic): \n %(value)",
-  answerUseCase: "You would use %(topic) in this case: \n%(value).",
+  answerUseCase: "You would use %(topic) in this case: \n%(value)."
 };
--- server.js
+++ server.js
@@ -17,8 +17,60 @@ var bot = new builder.BotConnectorBot(botConnectorOptions);
 
 bot.add('/', dialog);
 
-/** Answer help related questions like "what can I say?" */
 dialog.on('Help', builder.DialogAction.send(prompts.helpMessage));
+
+dialog.on('Hello', builder.DialogAction.send(prompts.helloMessage));
+
+dialog.on('Description', [askTopic, answerQuestion('description', prompts.answerDescription)]);
+
+dialog.on('Snippet', [askTopic, answerQuestion('snippet', prompts.answerSnippet)]);
+
+dialog.on('UseCase', [askTopic, answerQuestion('useCase', prompts.answerUseCase)]);
+
+function askTopic(session, args, next) {
+    // First check to see if we either got a topic from LUIS or have a an existing topic
+    // that we can multi-turn over.
+    var topic;
+    var entity = builder.EntityRecognizer.findEntity(args.entities, 'Topic');
+    if (entity) {
+        // The user specified a topic so lets look it up to make sure its valid.
+        // * This calls the underlying function Prompts.choice() uses to match a users response
+        //   to a list of choices. When you pass it an object it will use the intents as the
+        //   list of choices to match against. 
+        topic = builder.EntityRecognizer.findBestMatch(data, entity.entity);
+    } else if (session.dialogData.topic) {
+        // Just multi-turn over the existing Topic
+        topic = session.dialogData.topic;
+    }
+    
+    // Prompt the user to pick a topic if they didn't specify a valid one.
+    if (!topic) {
+        // Lets see if the user just asked for a topic we don't know about.
+        var txt = entity ? session.gettext(prompts.topicUnknown, { topic: entity.entity }) : prompts.topicUnknown;
+        
+        // Prompt the user to pick a topic from the list. They can also ask to cancel the operation.
+        builder.Prompts.choice(session, txt, data);
+    } else {
+        // Great! pass the Topic to the next step in the waterfall which will answer the question.
+        // * This will match the format of the response returned from Prompts.choice().
+        next({ response: topic })
+    }
+}
+
+function answerQuestion(field, answerTemplate) {
+    return function (session, results) {
+        // Check to see if we have a topic. The user can cancel picking a topic so IPromptResult.response
+        // can be null. 
+        if (results.response) {
+            // Save topic for multi-turn case and compose answer            
+            var topic = session.dialogData.topic = results.response;
+            var answer = { topic: topic.entity, value: data[topic.entity][field] };
+            session.send(answerTemplate, answer);
+        } else {
+            session.send(prompts.cancel);
+        }
+    };
+}
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -37,14 +89,14 @@ server.listen(process.env.port || 3978, function () {
 });
 
 var data = {
-  "Array": {
-    description: "An array is an ordered collection of objects. An array can contain several types of objects at once, such as integers, floats, strings, even other arrays or more complex objects.",
-    snippit: "[0, 4, 3, 'apple', [], 4.5]",
-    useCase: "An array is useful when you simply need to store a list of objects, without needing the added complexity of something like a hash."
+  'Array': {
+    description: 'An array is an ordered collection of objects. An array can contain several types of objects at once, such as integers, floats, strings, even other arrays or more complex objects.',
+    snippet: 'snippet',
+    useCase: 'An array is useful when you simply need to store a list of objects, without needing the added complexity of something like a hash.'
   },
-  "Hash": {
-    description: "A hash is a collection of key-value pairs.",
-    snippit: "{'a': 'apple', 'b': 'bear', 'c': ['camel', 'cabbage'], 'd': 42}",
-    useCase: "A hash is useful when you have a set of data that you want to organize by categories. In that case, you can set the categories as keys, and the data falling into that category can be the value."
+  'Hash': {
+    description: 'A hash is a collection of key-value pairs.',
+    snippet: 'hash snippet',
+    useCase: 'A hash is useful when you have a set of data that you want to organize by categories. In that case, you can set the categories as keys, and the data falling into that category can be the value.'
   }
-}
+};
